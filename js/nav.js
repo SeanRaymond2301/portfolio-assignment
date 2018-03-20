14eff7d1d1ff47d79c8d5bfc090305998c8ef3aa
SeanRaymond2301 <CarbomechGaming@gmail.com>
2018-03-20 14:26:58 -0700

    Never Realized the navigation wasn't fixed to the top of the page. GG

--- js/nav.js
+++ js/nav.js
@@ -6,16 +6,12 @@ function navBar() {
     header = document.getElementById("header");
 
     if(!boolVar) {
-        iconM.style.marginTop = "75px";
+        iconM.style.top = "75px";
         bar.style.top = "0";
-        header.style.marginTop = "-75px";
-        header.style.transitionDuration = "1.5s";
         boolVar = true;
     } else {
-        iconM.style.marginTop = "0";
+        iconM.style.top = "0";
         bar.style.top = "-75px";
-        header.style.transitionDuration = "1.5s";
-        header.style.marginTop = "0";
         boolVar = false;
     }
 }
