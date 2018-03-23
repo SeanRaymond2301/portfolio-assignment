ca6a82042358e24afabdc08a9e0a311a0fb25d85
SeanRaymond2301 <CarbomechGaming@gmail.com>
2018-03-23 15:48:08 -0700

    This carousel is frustrating me.

--- js/contactcards.js
+++ js/contactcards.js
@@ -4,70 +4,84 @@ var card = document.getElementsByClassName('w50'),
 
 function counter(n) {
     count = count + n;
+    if (count == 0) {
+        count = card.length - 1;
+    } else if (count == card.length - 1) {
+        count = 0;
+    }
 }
 
 function nextCard() {
 
-    if (count < card.length - 1) {
+
+
+    if (count < card.length) {
+
         for (let i = 0; i < card.length - 1; i++) {
             card[i].classList.remove("cardHiddenR");
             card[i].classList.remove("cardVisible");
-            card[i].classList.remove("transition");
+            card[i].classList.add("transition");
+        }
+
+        if (count >= 2) {
+            card[count - 2].classList.remove("transition");
         }
 
         if (count == 0) {
-            card[card.length - 2].classList.add("transition");
             card[card.length - 2].classList.add("cardHiddenR");
+            card[0].classList.add("cardVisible");
         }
 
         if (count >= 0) {
             card[count].classList.add("cardVisible");
-            card[count].classList.add("transition");
         }
 
-        if (count >= 1 && count < card.length - 1) {
-            card[count - 1].classList.add("transition");
+        if (count >= 1) {
             card[count - 1].classList.add("cardHiddenR");
         }
-        counter(1);
         console.log(count);
-    }
-        if (count == card.length - 1) {
-            counter(-card.length + 1);
+
+        if (count > card.length) {
+            card[count - 1].classList.add("cardHiddenR");
+            card[count - 2].classList.remove("cardHiddenR");
+            counter(-card.length - 1);
         }
+        counter(1);
+    }
 
 }
 
 function prevCard() {
-    if (count > 1) {
+    if (count > 0) {
+
         counter(-1);
         console.log(count);
-        if (!(count < 1)) {
+        if (count > 1) {
             for (let j = 0; j < card.length - 1; j++) {
                 card[j].classList.add("cardHiddenR");
                 card[j].classList.remove("cardVisible");
-                card[j].classList.remove("transition");
+                card[j].classList.add("transition");
             }
         }
+        card[0].classList.remove("cardHiddenR");
 
         if (count == 0) {
             card[0].classList.add("cardHiddenR");
-            card[0].classList.remove("cardVisible");
         }
 
         if (count > 0) {
             card[count - 1].classList.add("cardVisible");
             card[count - 1].classList.remove("cardHiddenR");
-            card[count - 1].classList.add("transition");
         }
 
         if (count <= card.length - 2) {
             card[count].classList.remove("cardVisible");
-            card[count].classList.add("transition");
             card[count].classList.remove("cardHiddenR");
         }
-    } if (count <= 1) {
+    }
+
+    if (count == 0) {
         card[0].classList.add("transition");
-        counter(card.length - 1);
+        counter(card.length);
     }
 }
