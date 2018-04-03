ac179a1b376c93d364d0b27a69c6c51a80385b90
SeanRaymond2301 <CarbomechGaming@gmail.com>
2018-04-03 15:29:10 -0700

    Nate fixed like, literally everything for me.

--- js/contactcards.js
+++ js/contactcards.js
@@ -1,57 +1,50 @@
 var card = document.getElementsByClassName('w50'),
     cardCL = card.classList,
-    count = 1;
+    count = 0;
 
-function counter(n) {
-    count = count + n;
-    if (count == 0) {
-        count = card.length - 1;
-    } else if (count == card.length - 1) {
-        count = 0;
-    }
+function initFirstCard() {
+    card[0].classList.add("cardVisible");
 }
 
 function nextCard() {
 
-
-
     if (count < card.length) {
-
+        count++;
         for (let i = 0; i < card.length - 1; i++) {
             card[i].classList.remove("cardHiddenR");
             card[i].classList.remove("cardVisible");
-            card[i].classList.add("transition");
-        }
-
-        if (count >= 2) {
-            card[count - 2].classList.remove("transition");
+            card[i].classList.remove("transition");
         }
 
         if (count == 0) {
+            card[card.length - 2].classList.remove("cardVisible");
             card[card.length - 2].classList.add("cardHiddenR");
             card[0].classList.add("cardVisible");
         }
 
         if (count >= 0) {
+            card[count].classList.add("transition");
             card[count].classList.add("cardVisible");
         }
 
         if (count >= 1) {
+            card[count - 1].classList.add("transition");
             card[count - 1].classList.add("cardHiddenR");
         }
         console.log(count);
 
-        if (count > card.length) {
-            card[count - 1].classList.add("cardHiddenR");
-            card[count - 2].classList.remove("cardHiddenR");
-            counter(-card.length - 1);
+        if (count == 6) {
+            card[0].classList.add("transition");
+            card[0].classList.add("cardVisible");
+            card[count].classList.add("transition");
+            card[count].classList.add("cardHiddenR");
+            count = 0;
         }
-        counter(1);
     }
 
 }
 
-function prevCard() {
+async function prevCard() {
     if (count > 0) {
 
         counter(-1);
@@ -82,6 +75,10 @@ function prevCard() {
 
     if (count == 0) {
         card[0].classList.add("transition");
-        counter(card.length);
+        counter(card.length - 1);
     }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
