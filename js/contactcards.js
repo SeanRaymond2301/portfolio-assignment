82a1478426f0a08072c2a4780ea6da2dc866c7ab
SeanRaymond2301 <CarbomechGaming@gmail.com>
2018-04-04 15:31:23 -0700

    Need to work on this a lot at home

--- js/contactcards.js
+++ js/contactcards.js
@@ -3,7 +3,9 @@ var card = document.getElementsByClassName('w50'),
     count = 0;
 
 function initFirstCard() {
-    card[0].classList.add("cardVisible");
+    classAdd(0, "cardHiddenR");
+    classAdd(1, "cardVisible");
+    count++;
 }
 
 function nextCard() {
@@ -11,74 +13,54 @@ function nextCard() {
     if (count < card.length) {
         count++;
         for (let i = 0; i < card.length - 1; i++) {
-            card[i].classList.remove("cardHiddenR");
-            card[i].classList.remove("cardVisible");
-            card[i].classList.remove("transition");
+            classRem(i, "cardHiddenR");
+            classRem(i, "cardVisible");
         }
 
         if (count == 0) {
-            card[card.length - 2].classList.remove("cardVisible");
-            card[card.length - 2].classList.add("cardHiddenR");
-            card[0].classList.add("cardVisible");
+            classRem(card.length - 2, "cardVisible");
+            classAdd(card.length - 2, "cardHiddenR");
+            classAdd(0, "cardVisible");
         }
 
         if (count >= 0) {
-            card[count].classList.add("transition");
-            card[count].classList.add("cardVisible");
+            classAdd(count, "cardVisible");
         }
 
         if (count >= 1) {
-            card[count - 1].classList.add("transition");
-            card[count - 1].classList.add("cardHiddenR");
+            classAdd(count - 1, "cardHiddenR");
         }
-        console.log(count);
 
         if (count == 6) {
-            card[0].classList.add("transition");
-            card[0].classList.add("cardVisible");
-            card[count].classList.add("transition");
-            card[count].classList.add("cardHiddenR");
+            classAdd(0, "cardVisible");
+            classAdd(count, "cardHiddenR")
             count = 0;
         }
     }
 
 }
 
-async function prevCard() {
-    if (count > 0) {
-
-        counter(-1);
-        console.log(count);
-        if (count > 1) {
-            for (let j = 0; j < card.length - 1; j++) {
-                card[j].classList.add("cardHiddenR");
-                card[j].classList.remove("cardVisible");
-                card[j].classList.add("transition");
-            }
+function prevCard() {
+    if (count >= 0) {
+        count--;
+        for (let j = 0; j < card.length - 1; j++) {
+            classRem(j, "cardHiddenR");
+            classRem(j, "cardVisible");
+            console.log(j);
         }
-        card[0].classList.remove("cardHiddenR");
-
-        if (count == 0) {
-            card[0].classList.add("cardHiddenR");
-        }
-
-        if (count > 0) {
-            card[count - 1].classList.add("cardVisible");
-            card[count - 1].classList.remove("cardHiddenR");
-        }
-
-        if (count <= card.length - 2) {
-            card[count].classList.remove("cardVisible");
-            card[count].classList.remove("cardHiddenR");
-        }
+        classAdd(count + 1, "cardHiddenR");
+        classAdd(count, "cardVisible");
     }
-
     if (count == 0) {
-        card[0].classList.add("transition");
-        counter(card.length - 1);
+        classAdd(0, "cardVisible");
+        count = card.length - 2;
     }
 }
 
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function classAdd(position, className) {
+    card[position].classList.add(className);
+}
+
+function classRem(position, className) {
+    card[position].classList.remove(className);
 }
