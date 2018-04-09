0d9a511f55900c41b6128234752f879fdab89627
Sean Raymond <carbomechgaming@gmail.com>
2018-04-08 18:57:58 -0700

    Changing how the nav bar is done so it can be styled better at small sizes

--- js/nav.js
+++ js/nav.js
@@ -6,12 +6,16 @@ function navBar() {
     header = document.getElementById("header");
 
     if(!boolVar) {
-        iconM.style.top = "75px";
-        bar.style.top = "0";
+        iconM.classList.add("down");
+//        iconM.style.top = "75px";
+        bar.classList.add("barDown");
+        //bar.style.top = "0";
         boolVar = true;
     } else {
-        iconM.style.top = "0";
-        bar.style.top = "-75px";
+        iconM.classList.add("up");
+//        iconM.style.top = "0";
+        bar.classList.add("barUp");
+//        bar.style.top = "-75px";
         boolVar = false;
     }
 }
